3031427d5180821f30def98490dace06a5cd95b8
Nick Tallant <ndtallant@uchicago.edu>
2019-01-27 20:17:21 -0600

    added view and unit test

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path #include
+from lists import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #url() for Django 1.x 
+    re_path(r'^$', views.home_page, name='home'),
 ]
